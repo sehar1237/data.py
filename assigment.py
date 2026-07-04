bb0231673edd77aada956e932b5820d7fe1f5259
SEHAR SHAHZAD <shahzadsehar403@gmail.com>
2026-07-04 16:33:45 +0500

    Implement book addition and display functions
    Added functionality to add and display books.

--- assigment.py
+++ assigment.py
@@ -1,1 +1,18 @@
-
+books=[]
+def add_book(books):
+  book_id=input("Enter Book ID;")
+  title=input("Enter Book Title")
+  author=input("Enter Author Name")
+  Quantity=int(input("Enter Quantity:"))
+  book={"id":book_id,
+        "title":title,
+        "author":author,
+        "quantity":quantity,}
+  books.append(book)
+  print("Book added sucessfully!")
+  def show books():
+    if len(books)==0:
+      print("no books avaliable")
+    else:
+      for book in books :
+        print()

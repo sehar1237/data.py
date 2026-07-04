c8741a76a001b4e9ce3f52a11474a502d99b7ff1
SEHAR SHAHZAD <shahzadsehar403@gmail.com>
2026-07-04 17:55:42 +0500

    Refactor add_book function and book data structure
    Updated the add_book function to include year and copies. Fixed syntax errors and improved book data structure.

--- assigment.py
+++ assigment.py
@@ -1,18 +1,26 @@
 books=[]
 def add_book(books):
-  book_id=input("Enter Book ID;")
-  title=input("Enter Book Title")
-  author=input("Enter Author Name")
-  Quantity=int(input("Enter Quantity:"))
-  book={"id":book_id,
-        "title":title,
-        "author":author,
-        "quantity":quantity,}
-  books.append(book)
-  print("Book added sucessfully!")
-  def show books():
-    if len(books)==0:
-      print("no books avaliable")
-    else:
-      for book in books :
-        print()
+  book_id=int (input("Enter Book ID:"))
+  title=input("Enter Book Title:")
+  author=input("Enter Author Name:"))
+  year=int(input("Enter Year:"))
+  books=[{"id":101,
+          "title":"Python Programming",
+          "author":"John Smith"
+          "year": 2023,
+          "copies":5}'
+           { "id" : 102,
+            "title" : "Data Struchers" ,
+            "author" :"Sara khan" ,
+             "year" : 2022,
+             "copies" : 5}
+           {"id" :103,
+           "title" : "Computer Networks" ,
+           "author" :" Ali Ahmad "
+            "year" : 2021,
+            "copies" : 5}]
+        
+    
+          
+           
+
